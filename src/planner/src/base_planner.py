f480ddedb6201d773ed55a8269fdbd53b51f4066
ChewKinWhye <Ckinwhye@hotmail.com>
2021-09-23 18:53:37 +0800

    Show graph

--- src/planner/src/base_planner.py
+++ src/planner/src/base_planner.py
@@ -220,11 +220,11 @@ class Planner:
             node = heapq.heappop(priority_queue)
             for action in actions:
                 next_state = self.discrete_motion_predict(node[2][0], node[2][1], node[2][2], action[0], action[1])
-                print(next_state)
                 if next_state is not None and next_state not in visited_states:
                     visited_states.add(next_state)
                     next_node = (self._d_from_goal(next_state)+node[1]+1, node[1]+1, next_state)
                     heapq.heappush(priority_queue, next_node)
+            break
         print(priority_queue)
 
     def get_current_continuous_state(self):
